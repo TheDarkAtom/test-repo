7cb2160522f33c3de64a48a966be3c5a5ab69480
DarkAtom <darkatom77@protonmail.com>
2026-06-16 19:31:59 +0300

    Fix
    Signed-off-by: DarkAtom <darkatom77@protonmail.com>

--- common.ps1
+++ common.ps1
@@ -3,7 +3,7 @@ param(
     [string]$Toolchain
 )
 
-$installDir     = "$PSScriptRoot/deps/install"
+$installDir = "$PSScriptRoot/deps/install"
 
 $cflags          = "-Wall -Wextra -fstack-protector-strong -ftrivial-auto-var-init=zero -g"
 $cflagsRelease   = "-O2 -DNDEBUG -D_FORTIFY_SOURCE=2"
@@ -11,18 +11,18 @@ $cxxflags        = "$cflags -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST
 $cxxflagsRelease = $cflagsRelease
 $ldflags         = ""
 
+$env:CFLAGS   = $cflags
+$env:CXXFLAGS = $cxxflags
+$env:LDFLAGS  = $ldflags
+
 $commonArgs = @(
     "-G", "Ninja"
     "-DCMAKE_BUILD_TYPE=Release"
     "-DCMAKE_TOOLCHAIN_FILE=$Toolchain"
     "-DCMAKE_INSTALL_PREFIX=$installDir"
-    "-DCMAKE_C_FLAGS=$cflags"
     "-DCMAKE_C_FLAGS_RELEASE=$cflagsRelease"
-    "-DCMAKE_EXE_LINKER_FLAGS=$ldflags"
-    "-DCMAKE_SHARED_LINKER_FLAGS=$ldflags"
 )
 
 $commonArgsCxx = @(
-    "-DCMAKE_CXX_FLAGS=$cxxflags"
     "-DCMAKE_CXX_FLAGS_RELEASE=$cxxflagsRelease"
 )
